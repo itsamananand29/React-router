ccd4c15ad9d2b3391861990ab9660aef2ba7b381
Aman Anand <itsamananand.29@gmail.com>
2023-01-13 23:12:26 +0000

    Added Authentication

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "./AuthContextProvider";
 function NavBar(props) {
+  const { isLoggedIn } = useAuthContext();
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="about">About</NavLink>
       <NavLink to="products">Products</NavLink>
       <NavLink to="users">Users</NavLink>
+      {!isLoggedIn && <NavLink to="login">Login</NavLink>}
     </nav>
   );
 }
